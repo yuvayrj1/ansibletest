a4a5f77254090893d83107a61e38f324637a6c8a
Yuvaraj <yuvaraj.u@aximsoft.com>
2019-12-24 15:51:12 +0530

    latest commit

--- test/integration/index.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest')
-
-const app = require('../../app')
-
-describe('Home page', () => {
-  test('it should render the landing page', () => {
-    return request(app).get('/').expect(200)
-  })
-})
